49e4c94a45cad29d4db13b9375788a9c0c1d2045
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-06-14 12:16:01 +0200

    update menu location

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-menu">
-    <b-dropdown id="dropdown-dropup" dropup text="Menu" variant="secondary" class="m-2">
+    <b-dropdown id="dropdown-dropup" dropup text="Menu" variant="primary" class="m-2">
         <b-dropdown-item to="/">Map</b-dropdown-item>
         <b-dropdown-item to="/project/">List</b-dropdown-item>
         <b-dropdown-item to="/about">About</b-dropdown-item>
@@ -18,7 +18,8 @@ export default {
 .main-menu {
   position: fixed;
   bottom: 1rem;
-  left: 1rem;
+  top: calc(100vh - 4rem);
   z-index: 999;
+  filter: drop-shadow(4px 4px 4px rgb(25, 25, 25));
 }
 </style>
