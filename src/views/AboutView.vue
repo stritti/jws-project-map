f68ee6c2b6e877f7f5bdaee2b538761aeec10e82
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-07-13 10:41:16 +0200

    updated about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,41 @@
 <template>
   <b-container class="about">
-    <h1>This is Project</h1>
-    <p>TBD</p>
+    <h1>About this App</h1>
+    <p>
+      All projects of the Jörg Wolff Foundation can be found in the this
+      overview map. All buildings already completed are marked with colored pins.
+      The planned projects are marked in gray.
+    </p>
+    <h3>Development</h3>
+    <p>
+      Stephan Strittmatter
+    </p>
+    <p>
+      Sources:
+      <a href="https://github.com/stritti/jws-project-map" target="_blank">
+        GitHub
+      </a>
+    </p>
+
+    <h3>Credits</h3>
+    <p>
+      <ul>
+        <li>
+          <a href="https://airtable.com" target="_blank">Airtable</a>
+        </li>
+        <li>
+          <a href="https://vuejs.org" target="_blank">vue.js</a>, MIT License
+        </li>
+        <li>
+          <a href="https://leafletjs.com/" target="_blank">Leaflet</a>, OpenStreetMap
+        </li>
+        <li>
+          and more
+          <a href="https://github.com/stritti/jws-project-map/blob/main/package.json" target="_blank">
+            awesome libs &hellip;
+          </a>
+        </li>
+      </ul>
+    </p>
   </b-container>
 </template>
