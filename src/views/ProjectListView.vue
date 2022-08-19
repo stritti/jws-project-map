9e3d381e328a5177e956ee06079f6d52e769cabf
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-08-19 10:04:00 +0200

    improved filter

--- src/views/ProjectListView.vue
+++ src/views/ProjectListView.vue
@@ -1,37 +1,73 @@
 <template>
   <b-container>
     <div class="project-list">
-      <h1>Project Overview</h1>
+      <h1>JWF + Humanaktiv: Project Overview</h1>
 
-      <b-skeleton-wrapper :loading="loading">
+      <b-skeleton-wrapper :loading="loading > 0">
         <template #loading>
-          <h1><b-skeleton width="75%"></b-skeleton></h1>
+          <h3><b-skeleton width="75%"></b-skeleton></h3>
           <p>
             <b-skeleton width="85%"></b-skeleton>
             <b-skeleton width="55%"></b-skeleton>
             <b-skeleton width="70%"></b-skeleton>
           </p>
         </template>
-        <p>
+
+        <h3 class="my-3">
           Projects: {{ projectCount }}
           (including: {{ projectsUnderConstruction.length }} under construction,
           {{ projectsPlanned.length }} planned)
-        </p>
-        <b-form-group label="Project State Filter">
-          <b-form-checkbox-group
-            v-model="stateFilter"
-            :options="stateOptions"
-          >
-          </b-form-checkbox-group>
-        </b-form-group>
-        <b-card-group>
-          <project-list-item
-            v-for="project in filteredProjectList"
-            v-bind:key="project.id"
-            :project="project"
-          />
-        </b-card-group>
+        </h3>
+
+        <b-button v-b-toggle.collapse-filter variant="primary">
+          <bootstrap-icon icon="filter"/>
+          Filter
+        </b-button>
+        <b-collapse id="collapse-filter">
+          <b-card bg-variant="light" class="mb-5">
+            <b-form-group label="Project State:">
+              <b-form-checkbox-group
+                name="stateFilter"
+                v-model="stateFilter"
+                :options="stateOptions"
+                size="sm"
+              >
+              </b-form-checkbox-group>
+            </b-form-group>
+            <b-form-group label="Project Category:">
+              <b-form-checkbox-group
+                v-model="categoryFilter"
+                :options="categoryList"
+                text-field="name"
+                value-field="id"
+                size="sm"
+              >
+              </b-form-checkbox-group>
+            </b-form-group>
+            <b-form-group label="Country:">
+              <b-form-checkbox-group
+                v-model="countryFilter"
+                :options="countryList"
+                text-field="name"
+                value-field="id"
+                size="sm"
+              >
+              </b-form-checkbox-group>
+            </b-form-group>
+            <h3 class="mt-3">
+              {{ filteredProjectList.length }} filtered Projects
+            </h3>
+          </b-card>
+        </b-collapse>
       </b-skeleton-wrapper>
+
+      <b-card-group columns class="my-3">
+        <project-list-item
+          v-for="project in filteredProjectList"
+          v-bind:key="project.id"
+          :project="project"
+        />
+      </b-card-group>
     </div>
   </b-container>
   <site-footer />
@@ -41,12 +77,15 @@
 import { mapState } from 'pinia'
 import { useLoadingStore } from '@/store/loading.store'
 import { useProjectStore } from '@/store/project.store'
+import { useCategoryStore } from '@/store/category.store'
+import { useCountryStore } from '@/store/country.store'
 
+import BootstrapIcon from '@dvuckovic/vue3-bootstrap-icons'
 import ProjectListItem from '@/components/project/ProjectListItem.vue'
 import SiteFooter from '@/components/SiteFooter.vue'
 
 export default {
-  components: { ProjectListItem, SiteFooter },
+  components: { BootstrapIcon, ProjectListItem, SiteFooter },
   name: "ProjectDetailsView",
   data() {
     return {
@@ -55,27 +94,45 @@ export default {
         { text: 'finished', value: 'finished' },
         { text: 'under construction', value: 'under construction' },
         { text: 'planned', value: 'planned' }
-      ]
+      ],
+      categoryFilter: [],
+      countryFilter: [],
     }
   },
   computed: {
     ...mapState( useLoadingStore, {
-      loading: 'showLoadingSpinner'
+      loading: 'loading'
     }),
     ...mapState( useProjectStore, {
       projectList: 'projects'
     }),
-    projectCount () {
-      return this.projectList.length
-    },
+    ...mapState( useCategoryStore, {
+      categoryList: 'categories'
+    }),
+    ...mapState( useCountryStore, {
+      countryList: 'countries'
+    }),
     filteredProjectList () {
-      let filteredList = []
+      const loadingStore = useLoadingStore()
+      loadingStore.updateLoading(true)
+      const filteredList = []
+
       for ( let i = 0; i < this.projectList.length; i++ ) {
-        if ( this.stateFilter.includes(this.projectList[i].state) ) {
-          filteredList.push(this.projectList[i])
+        const project = this.projectList[i]
+        if (
+          (this.stateFilter.length === 0 || this.stateFilter.includes(project.state)) &&
+          (this.categoryFilter.length === 0 || this.categoryFilter.some(r => project.category.includes(r))) &&
+          (this.countryFilter.length === 0 || this.countryFilter.includes(project.country[0]))
+        ) {
+          filteredList.push(project)
         }
       }
+
+      loadingStore.updateLoading(false)
       return filteredList
+    },
+    projectCount () {
+      return this.projectList.length
     },
     projectsFinished () {
       if (this.projectList.length > 0) {
