dd6c4fe7a792c8fe2ddbd8c4e1b5f0538421b70b
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2025-02-11 22:26:54 +0100

    chore: terser options removed

--- vite.config.ts
+++ vite.config.ts
@@ -68,13 +68,6 @@ export default defineConfig({
     },
   },
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
     rollupOptions: {
       output: {
         manualChunks(id) {
