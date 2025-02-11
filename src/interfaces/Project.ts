3a9a0da5538891219e644bd7c965452b8fa383dc
Stephan Strittmatter <stritti@users.noreply.github.com>
2025-02-11 11:59:12 +0100

    Feature: map pinch zoom (#592)
    * feat: Enable mouse wheel and touch zoom on location map
    
    * chore: Attachment interface added

--- src/interfaces/Project.ts
+++ src/interfaces/Project.ts
@@ -1,27 +1,16 @@
+import { Attachment } from "./Attachment";
+
 export interface Project {
   id: number;
   name: string;
   category: Array<number>;
   country: number;
-  teaserImg?: Array<{
-    mimetype: string;
-    signedUrl: string;
-    name: string;
-  }>;
+  teaserImg?: Array<Attachment> | undefined;
   state: string;
   notes: string | undefined;
   latitude: number;
   longitude: number;
   link: string | undefined;
   since?: Date | null;
-  gallery?: Array<{
-    mimetype: string;
-    signedUrl: string;
-    thumbnails?: {
-      card_cover?: {
-        signedUrl: string;
-      };
-    };
-    name: string;
-  }> | undefined;
+  gallery?: Array<Attachment> | undefined;
 }
