b81ba6d5eaf7f51ab76d3acbc0a8463bcb6f2c83
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2026-03-12 08:52:58 +0100

    chore: Delete `projectDataWorker.js` as it is no longer used and direct processing is faster for initial loading.

--- src/services/projectDataWorker.js
+++
@@ -1,6 +0,0 @@
-// Dieser Worker wird nicht mehr verwendet, da die direkte Verarbeitung schneller ist
-// für das initiale Laden. Der Code bleibt als Referenz erhalten.
-
-self.onmessage = function(e) {
-  self.postMessage([]);
-};
